e8d11de8abca238fac44ac9e5db3017c83159e89
josue <jehs2407@gmail.com>
2026-05-08 13:37:39 -0600

    front

--- Dashboard/routes/api.php
+++ Dashboard/routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\RentailController;
 use App\Http\Controllers\Api\AuthController;
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
---
+++ car_rentail/src/interfaces/car.tsx
@@ -0,0 +1,18 @@
+export default interface Car {
+    id:number,
+    brand_id:string,
+    models:string,
+    year:string,
+    color:string,
+    licence_plate:string,
+    mileage:number,
+    lat:number,
+    lng:number,
+    Is_premium:boolean,
+    created_at:string,
+    rentail_count:number,
+    daly_rate:number,
+    status:string,
+    updated_at:string
+    
+}
